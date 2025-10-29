ffd3ef464119be1fa552f2da909905c07a27e79a
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-10-29 07:55:15 +0700

    Add --force to everything.

--- DANGER-DOCKER-WIPEOUT.sh
+++ DANGER-DOCKER-WIPEOUT.sh
@@ -1,16 +1,16 @@
 #/bin/bash
 
 # Remove all stopped containers.
-docker container prune
+docker container prune --force
 
 # Remove all containers.
-docker rm $(docker ps -aq)
+docker rm $(docker ps -aq) --force
 
 # Delete all images.
 docker rmi $(docker images -q) --force
 
 # Remove stopped containers and images, including unused and dangling images.
-docker system prune -a
+docker system prune -a --force
 
 # Delete all volumes.
-docker volume rm $(docker volume ls -q)
+docker volume rm $(docker volume ls -q) --force
